7e42c6d51c236060785f96c927428db2d36fd778
Rui Pedro de Azevedo Camposinhos <rcamposinhos@Ruis-MBP.lan>
2022-04-11 17:26:32 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hi there! Volumes working :)</h1>
+        <h2>Changed src</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
